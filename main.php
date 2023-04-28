6efa8642a336c7596244164a9739f57e821c7d54
luistarkbank <luiz.almeida@starkbank.com>
2023-04-28 19:14:15 -0300

    teste

--- main.php
+++ main.php
@@ -1,7 +1,12 @@
 <?php
+<<<<<<< HEAD
 print_r("hejsklajdklsallo");
 print_r("hello wjfkldsorld");
 
 print_r("hello wjfkldsorld");
+=======
+print_r("hello");
+print_r("hello wjfkldsorld")
+>>>>>>> 36beaba (comitado)
 
 ?>
